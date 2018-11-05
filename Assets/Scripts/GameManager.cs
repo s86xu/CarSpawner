fec668a9d470d40f3618f1162d9cc1aa426a05ac
Jayson Su <s86xu@edu.uwaterloo.ca>
2018-11-05 14:59:42 -0500

    Imported Photon Networking and Starting Managers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace xyz.sharkle.CarSpawner {
+
+	public class GameManager : MonoBehaviour {
+
+		// References.
+		// [HideInInspector]
+		
+		
+		PhotonManager photonManager;
+
+		// Reference setting... 
+		void Awake () {
+			photonManager = gameObject.AddComponent<PhotonManager>();
+		}
+
+		// Game Initialization...
+		void Start () {
+			photonManager.ConnectToMaster("us", "Player#1");
+		}
+		
+		// Update is called once per frame
+		// void Update () {
+			
+		// }
+	}
+}
---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+/*
+	This class is a wrapper around PhotonNetwork and everything Photon.
+
+	The reasoning is that to modulize the references so PhotonNetwork doesn't scatter
+	everywhere throughout the code. If this were to be replaced by another networking
+	modules it could be done easily by editing this class.
+*/
+
+namespace xyz.sharkle.CarSpawner {
+	public class PhotonManager : MonoBehaviourPunCallbacks {
+
+		List<string> MasterServerList;
+
+		void Awake () {
+			MasterServerList = new List<string>(PhotonNetwork.BestRegionSummaryInPreferences.Split(';')[2].Split(','));
+		}
+
+		// Returns a list of server code you could potentially connect.
+		public List<string> GetListOfMasterServer () {
+			return MasterServerList;
+		}
+
+		// Get the Default preferred master server that is cloestest to the client.
+		public string GetPreferredMasterServer () {
+			return PhotonNetwork.BestRegionSummaryInPreferences.Split(';')[0];
+		}
+
+		public void ConnectToMaster (string ServerCode, string PlayerName) {
+			PhotonNetwork.NickName = PlayerName;
+			PhotonNetwork.PhotonServerSettings.AppSettings.FixedRegion = ServerCode;
+			PhotonNetwork.ConnectUsingSettings();
+		}
+
+		public override void OnConnectedToMaster() {
+			Debug.Log("Connected to: " +  PhotonNetwork.CloudRegion);
+		}
+
+	}
+}
